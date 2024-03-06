0dd82aac3ef673c71061197ad50ba137a208b027
challenger1736 <challenger1736@gmail.com>
2024-03-06 19:07:15 +0900

    commit

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,7 @@
 /* 공통 js */
 
 function toggleMenu() { /* 햄버거 토글기능 넣기 */
-     menu = document.querySelector('.hamburgerMenu');
+    let menu = document.querySelector('.hamburgerMenu');
     menu.classList.toggle('active');
 }
+
