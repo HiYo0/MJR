ac1741a003ab7a4fb984a6b3f66ae1dd9bbd4baf
challenger1736 <challenger1736@gmail.com>
2024-03-07 01:51:58 +0900

    commit

---
+++ src/main/java/project1/controller/AdminController.java
@@ -0,0 +1,14 @@
+package project1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+//    @GetMapping("/main/admin") // 회원 목록 전체 출력
+//    public String adminView(){
+//
+//    }
+
+}
--- src/main/resources/static/js/index.js
+++
@@ -1,7 +0,0 @@
-/* 공통 js */
-
-function toggleMenu() { /* 햄버거 토글기능 넣기 */
-    let menu = document.querySelector('.hamburgerMenu');
-    menu.classList.toggle('active');
-}
-
