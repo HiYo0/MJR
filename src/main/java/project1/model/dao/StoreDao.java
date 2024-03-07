1ec4db4dfcf5b1e9bab15f394225ae3622e79e3d
geonwk95 <abc777z@naver.com>
2024-03-07 18:08:02 +0900

    김병래 0307 18:07 전체 출력 중

--- src/main/java/project1/model/dao/StoreDao.java
+++ src/main/java/project1/model/dao/StoreDao.java
@@ -3,17 +3,20 @@ package project1.model.dao;
 import org.springframework.stereotype.Component;
 import project1.model.dto.StoreDto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class StoreDao extends Dao{
+public class StoreDao extends Dao {
     public long doPostStoreReg(StoreDto storeDto) {
         System.out.println("StoreDao.doPostStoreReg");
         System.out.println("storeDto = " + storeDto);
         try {
-            String sql=
+            String sql =
                     "insert into store(sname,sphone,simg1,simg2,simg3,simg4,sadress,scontent,snumber,categorya,categoryb,mno) " +
                             " value(?,?,?,?,?,?,?,?,?,?,?,?)";
-            ps=conn.prepareStatement(sql);
-            ps.setString(1,storeDto.getSname());
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, storeDto.getSname());
             ps.setString(2, storeDto.getSphone());
             ps.setString(3, storeDto.getSfile1());
             ps.setString(4, storeDto.getSfile2());
@@ -21,26 +24,74 @@ public class StoreDao extends Dao{
             ps.setString(6, storeDto.getSfile4());
             ps.setString(7, storeDto.getSadress());
             ps.setString(8, storeDto.getScontent());
-            ps.setString(9,storeDto.getSnumber());
-            ps.setInt(10,storeDto.getCategorya());
-            ps.setInt(11,storeDto.getCategoryb());
-            ps.setLong(12,storeDto.getMno());
-            int count=ps.executeUpdate();
-            if(count==1){
+            ps.setString(9, storeDto.getSnumber());
+            ps.setInt(10, storeDto.getCategorya());
+            ps.setInt(11, storeDto.getCategoryb());
+            ps.setLong(12, storeDto.getMno());
+            int count = ps.executeUpdate();
+            if (count == 1) {
                 System.out.println("등록성공");
                 return 1;
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("e = " + e);
         }
         return 0;
     }
 
     //2. 가게 전체 출력
-    /*public StoreDto doGetStoreList(){
+    public List<StoreDto> dogetStoreViewList(int startrow, int pageBoardSize, int categorya, int categoryb, String key, String keyword) {
         System.out.println("StoreController.doGetStoreList");
-        return ;
-    }*/
+        StoreDto storeDto = null;
+        List<StoreDto> list = new ArrayList<>();
+        try {
+            String sql = "select * from store " ;
+            //================1. 만약에 카테고리 조건이 있으면 where 추가.
+            if(categorya==1){
+                if(categoryb>0){sql+=" where categoryb ="+categoryb;}
+            }else if(categorya>1){
+                sql+=" where categorya ="+categorya;
+                if(categoryb>0){sql+=" and categoryb ="+categoryb;}
+            }
+            //================2. 만약에 검색 있을때
+            if(!keyword.isEmpty()){   System.out.println("검색 키워드가 존재");
+                if(categorya!=1|| categoryb!=0){sql+=" and ";}   // 카테고리가 있을 때, and 로 연결
+                else{sql += " where ";}       // 카테고리가 없을 때, where 로 연결
+                sql+= key+" like '%"+keyword+"%' ";
+            }
+            sql+=" order by b.bdate desc " +
+                    " limit ?, ?";
 
+            ps=conn.prepareStatement(sql);
+            ps=conn.prepareStatement(sql);
+            ps.setInt(1,startrow);
+            ps.setInt(2,pageBoardSize);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                storeDto= new StoreDto(rs.getLong("sno"),
+                        rs.getString("sname"),rs.getString("sphone"),
+                        rs.getString("sadress"),rs.getString("scontent"),
+                        rs.getInt("sstate"),rs.getString("snumber"),
+                        rs.getInt("categorya"),rs.getInt("categoryb"),
+                        rs.getString("simg1"), rs.getString("simg2"),
+                        rs.getString("simg3"), rs.getString("simg4"),
+                        null,null,null,null,rs.getLong("mno")
+                        );
+                        list.add(storeDto);
+                System.out.println("sql = " + sql);
+                System.out.println("list = " + list);
+            }
+
+        } catch (Exception e) {
+            System.out.println("e = " + e);
+        }
+
+        return list;
+    }
+
+    //2-2. 전체 게시물 수 호출
+    public int getStoreSize(int categorya, int categoryb, String field, String value) {
+        return 0;
+    }
 }
--- src/main/resources/static/js/store.js
+++ src/main/resources/static/js/store.js
@@ -1,4 +1,11 @@
-
+let pageObject = {
+    page: 1,                            // 현재 페이지
+    pageBoardSize: 5,                   // 페이지당 표시할 게시물 수
+    categorya : 1,                             // 현재 카테고리1
+    categoryb : 0,                              // 현재 카테고리2
+    key: 'b',                    //현재 검색 key
+    keyword: ''                         // 현재 검색 keyword
+};
 
 //1. 가게 등록
 function onReg(){
@@ -28,5 +35,29 @@ function onReg(){
         }
     })
 }
-//1-1 카테고리번호 가져오기
-function getCategory(){}
+//2. 전체 출력
+function storeView(page){console.log("storeView()");
+
+    pageObject.page=page;
+    $.ajax({
+        url:"store/do",
+        method: "get",
+        data: pageObject,
+        success: (r)=>{console.log(r);
+
+        //1. 어디에
+        let storeList=document.querySelector('.storeList');
+        //2. 무엇을
+        let html="";
+            //서버가 보여준 데이터를 출력
+            //1.
+            r.list.forEach(store)=>{}
+
+
+        }
+
+
+    })
+
+
+}
