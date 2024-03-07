9c5b3524941fd4c9be2a1628e1edbf0690b3db5f
challenger1736 <challenger1736@gmail.com>
2024-03-07 18:32:45 +0900

    20240307 시작 커밋

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,9 +1,9 @@
 // 관리자 페이지로 들어가면 나올 js
 // 관리자 자기 데이터 및 세션에 관리자 저장.
+let tablerows = 7;
 
-
-adminMview()
-function adminMview(){ // 전체
+adminMview(tablerows);
+function adminMview(tablerows){ // 전체
     console.log('adminMview() 실행, 전체회원불러오기')
     $.ajax({
         url : "/admin/Mview",
@@ -14,18 +14,22 @@ function adminMview(){ // 전체
             let adminMtable = document.querySelector("#adminMtable>tbody");
             // 무엇을
             let html = "";
-            r.forEach((member1)=>{
-                let daytime = member1.mdate.split(" ");
-                html += `
-                        <tr>
-                           <th>${member1.mno}</th>
-                           <th>${member1.mid}</th>
-                           <th>${member1.mname}</th>
-                           <th>daytime[0]</th>
-                           <th>${member1.mstate}</th>
-                       </tr>
-                        `
-            })
+            for(let i =0 ; i<tablerows ; i++){
+            let daytime = r[i].mdate.split(" ");
+                            if(r[i].mstate == 0){r[i].mstate = "일반"}
+                            else if(r[i].mstate == 1){r[i].mstate = "정지"}
+                            else if(r[i].mstate == 2){r[i].mstate = "탈퇴"}
+                            else if(r[i].mstate == 3){r[i].mstate = "관리자"}
+                            html += `
+                                    <tr>
+                                       <th>${r[i].mno}</th>
+                                       <th>${r[i].mid}</th>
+                                       <th>${r[i].mname}</th>
+                                       <th>${daytime[0]}</th>
+                                       <th>${r[i].mstate}</th>
+                                   </tr>
+                                    `}
+
             adminMtable.innerHTML = html;
 
         }
