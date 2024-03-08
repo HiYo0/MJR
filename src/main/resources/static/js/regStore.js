9325a6972f1098142ad02eab3c7f09bd55a5c3e3
qudfoqjf <c8571@naver.com>
2024-03-08 21:33:08 +0900

    1

---
+++ src/main/resources/static/js/regStore.js
@@ -0,0 +1,28 @@
+//1. 가게 등록
+function onReg(){
+    console.log("onReg()");
+    //1. 폼DOM 가져온다.
+    let storeRegForm = document.querySelector('.storeRegForm');
+    console.log(storeRegForm);
+    //2. 폼 바이트 (바이너리) 객체 변환
+    let storeRegFormData= new FormData(storeRegForm);
+    console.log(storeRegFormData);
+    //3. ajax 첨부파일 폼 전송
+    $.ajax({
+        url : "/store/reg.do" ,
+        method : "post" ,
+        data: storeRegFormData,
+        contentType: false,
+        processData: false,
+        success : (r) => {
+            console.log(r);
+            if(r==0){
+                alert('등록 실패: 관리자에게 문의: DB오류')
+            }else if(r==-1){
+                alert('등록실패: 관리자에게 문의 (첨부파일 오류)')
+            }else if(r>=1){
+            alert('등록 성공')
+            }
+        }
+    })
+}
--- src/main/resources/static/js/store.js
+++ src/main/resources/static/js/store.js
@@ -8,34 +8,7 @@ let pageObject = {
 };
 const categoryLista=['1','자유','안산','시흥','수원','부천','안양','서울'];
 const categoryListb=['1','한식','일식','중식','양식','분식','패스트푸드'];
-//1. 가게 등록
-function onReg(){
-    console.log("onReg()");
-    //1. 폼DOM 가져온다.
-    let storeRegForm = document.querySelector('.storeRegForm');
-    console.log(storeRegForm);
-    //2. 폼 바이트 (바이너리) 객체 변환
-    let storeRegFormData= new FormData(storeRegForm);
-    console.log(storeRegFormData);
-    //3. ajax 첨부파일 폼 전송
-    $.ajax({
-        url : "/store/reg.do" ,
-        method : "post" ,
-        data: storeRegFormData,
-        contentType: false,
-        processData: false,
-        success : (r) => {
-            console.log(r);
-            if(r==0){
-                alert('등록 실패: 관리자에게 문의: DB오류')
-            }else if(r==-1){
-                alert('등록실패: 관리자에게 문의 (첨부파일 오류)')
-            }else if(r>=1){
-            alert('등록 성공')
-            }
-        }
-    })
-}
+
 storeView(1);
 //2. 전체 출력
 function storeView(page){
